cf852d562ef1d983b630209dec02154b50021f1b
Alfredo Chusgo <alfredochusgodev@gmail.com>
2023-07-15 14:41:32 +0000

    added import statements in a simple test

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
 import { sum } from './sum.js';
+import { describe, expect, test } from '@jest/globals';
 
-test('adds 1 + 2 to equal 3', () => {
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
+});
